267e184615fba8608b77a6eb95fc999dc3c34b4c
ansarialiakbar <ansarialiakbar2001@gmail.com>
2025-05-07 01:42:38 +0530

    signup logic with password encryption done

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
 const {PORT} = require('./config/serverConfig')
+const apiRoiutes = require('./routes/index')
 
 const app = express()
 
 const prepareAndStartServer = () => {
+    app.use(bodyParser.json())
+    app.use(bodyParser.urlencoded({extended: true}))
+    app.use('/api', apiRoiutes)
 
     app.listen(PORT, () => {
         console.log(`Server running at ${PORT}`);
---
+++ src/repository/user-repository.js
@@ -0,0 +1,31 @@
+const {User} = require('../models/index')
+
+class UserRepository{
+
+    async create(data){
+        try {
+            const user = await User.create(data)
+            return user   
+        } catch (error) {
+            console.log("something wrong in repository layer");
+            throw error;    
+        }
+    }
+
+    async delete(userId){
+        try {
+             await User.destroy({
+                where:{
+                    id: userId
+                }
+            })
+            return true;   
+        } catch (error) {
+            console.log("something wrong in repository layer");
+            throw error;    
+        }
+    }
+
+}
+
+module.exports = UserRepository
