14108122eb7d13754abde08769342e841412686d
ansarialiakbar <ansarialiakbar2001@gmail.com>
2025-05-07 02:17:13 +0530

    added get user by id in repo

--- src/repository/user-repository.js
+++ src/repository/user-repository.js
@@ -26,6 +26,19 @@ class UserRepository{
         }
     }
 
+    async getById(userId){
+        try {
+            const user = await User.findByPk(userId, {
+                attributes: ['email', 'id']
+            })
+            return user
+            
+        } catch (error) {
+            console.log("something wrong in repository layer");
+            throw error;  
+        }
+    }
+
 }
 
 module.exports = UserRepository
