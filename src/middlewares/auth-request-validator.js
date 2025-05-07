721638dd24bd43a4c6b1a2af3d50deef5c463a39
ansarialiakbar <ansarialiakbar2001@gmail.com>
2025-05-08 01:49:23 +0530

    added isAdmin api

--- src/middlewares/auth-request-validator.js
+++ src/middlewares/auth-request-validator.js
@@ -3,13 +3,27 @@ const validateUserAuth = (req, res, next) => {
         return res.status(400).json({
             data: {},
             success: false,
-            message: 'Somethinf went wrong',
+            message: 'Something went wrong',
             err: 'Email or Password missing in the request'
         })
     }
     next()
 }
 
+const validateIsAdminRequest = (req, res, next)=>{
+    if(!req.body.id){
+        return res.status(400).json({
+            data: {},
+            success: false,
+            message: 'Something went wrong',
+            err: 'user id is not given'
+        })
+       
+    }
+    next()
+}
+
 module.exports = {
-    validateUserAuth
+    validateUserAuth,
+    validateIsAdminRequest
 }
