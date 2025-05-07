b8554a69c67db001dfbd814b9c20d93eb85e6377
ansarialiakbar <ansarialiakbar2001@gmail.com>
2025-05-07 12:00:24 +0530

    added jwt create and verify logic

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser')
 const {PORT} = require('./config/serverConfig')
 const apiRoiutes = require('./routes/index')
 
+// const UserService = require('./services/user-service')
+
 const app = express()
 
 const prepareAndStartServer = () => {
@@ -13,6 +15,19 @@ const prepareAndStartServer = () => {
 
     app.listen(PORT, () => {
         console.log(`Server running at ${PORT}`);
+
+        // const service = new UserService()
+        // const newToken = service.createToken({
+        //     email: 'akbar@admin.com',
+        //     id: 1
+        // })
+        // console.log('new Token is', newToken);
+
+        // const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFrYmFyQGFkbWluLmNvbSIsImlkIjoxLCJpYXQiOjE3NDY1OTc4MTUsImV4cCI6MTc0NjYwMTQxNX0.lCKt8ypy7tQZmEk2mPExWFRC5UDR1ak4A6D-7DUAcYw'
+        // const response = service.verifyToken(token)
+        // console.log(response);
+        
+        
         
     })
 
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,4 +1,7 @@
+const jwt = require('jsonwebtoken')
+
 const UserRepository = require('../repository/user-repository')
+const {JWT_KEY} = require('../config/serverConfig')
 
 class UserService{
     constructor(){
@@ -16,6 +19,29 @@ class UserService{
             
             
         }
+    }
+
+    createToken(user){
+        try {
+            const result = jwt.sign(user, JWT_KEY, {expiresIn: '1d'} )
+            return result
+        } catch (error) {
+            console.log("something went wrong in token creation")
+            throw error
+            
+        }
+    }
+
+    verifyToken(token){
+        try {
+            const response = jwt.verify(token, JWT_KEY )
+            return response
+        } catch (error) {
+            console.log("something went wrong in token validation")
+            throw error
+            
+        }
+
     }
 
 }
